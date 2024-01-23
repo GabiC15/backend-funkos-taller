c74d3885cdd158691f640ec98ccd950c9677488b
ffernandojp <ffernandoj.perez@gmail.com>
2024-01-23 15:58:45 -0300

    uploading filtering category

--- src/graphql/resolvers/categoria.js
+++ src/graphql/resolvers/categoria.js
@@ -2,8 +2,14 @@ import Categoria from "./../../db/models/categoria.js";
 
 export default {
   Query: {
-    categorias: () => Categoria.findAll({ include: "padre" }),
+    categorias: () =>
+      Categoria.findAll({ include: "padre", where: { padre_id: null } }),
     categoria: (parent, args) =>
       Categoria.findByPk(args.id, { include: "padre" }),
+    subcategorias: (_, args) =>
+      Categoria.findAll({
+        include: "padre",
+        where: { padre_id: args.categoriaId },
+      }),
   },
 };
