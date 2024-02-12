9121d21b677ff051dd4f5cf53bbe09060c6a98c7
Gabriel <luisgabrielcasares@gmail.com>
2024-02-12 16:14:41 -0300

    Add envs

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log(process.env.NODE_ENV);
 dotenv.config({
   path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV}`),
 });
