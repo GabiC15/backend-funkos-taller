0ec10241cfcb988805da2c037d211bb388ead587
Gabriel <luisgabrielcasares@gmail.com>
2024-02-16 10:02:10 -0300

    Change rol to createUsuario

--- src/db/models/pedido.js
+++ src/db/models/pedido.js
@@ -28,6 +28,11 @@ export default class Pedido extends Model {
           type: DataTypes.BOOLEAN,
           allowNull: true,
         },
+        commissionCost: {
+          type: DataTypes.REAL,
+          field: "commission_cost",
+          allowNull: true,
+        },
         usuarioId: {
           field: "usuario_id",
           type: DataTypes.INTEGER,
