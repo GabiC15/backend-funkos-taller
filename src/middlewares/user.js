9bf33e068dc913fb657c98306de31de86d202f0a
Gabriel <luisgabrielcasares@gmail.com>
2024-02-19 22:20:10 -0300

    Add try catch jwt decode

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,17 +1,21 @@
 import { getAuth } from "firebase-admin/auth";
 import jwt from "jsonwebtoken";
 
-const userMiddleware = async (req, _, next) => {
+const userMiddleware = async (req, res, next) => {
   const { session } = req.cookies || req.headers;
 
   if (session) {
-    const decodedCookie = jwt.verify(session, process.env.JWT_SECRET);
+    try {
+      const decodedCookie = jwt.verify(session, process.env.JWT_SECRET);
 
-    req.usuario = {
-      id: decodedCookie.id,
-      uid: decodedCookie.user_id,
-      rol: decodedCookie.rol,
-    };
+      req.usuario = {
+        id: decodedCookie.id,
+        uid: decodedCookie.user_id,
+        rol: decodedCookie.rol,
+      };
+    } catch (e) {
+      res.sendStatus(401);
+    }
   }
 
   next();
